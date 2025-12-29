7245084e8246139d8e77a51a90a3fec2ed6736d6
Joseph <joscast5@hotmail.com>
2025-12-29 08:41:38 -0500

    Clean up Domain project and add Reminder model

---
+++ ReminderApp.Domain/Reminder.cs
@@ -0,0 +1,10 @@
+﻿namespace ReminderApp.Domain;
+
+public class Reminder
+{
+    public Guid Id {get; set;} = Guid.NewGuid();
+    public string Title {get; set;} = string.Empty;
+    public string? Description {get; set;}
+    public DateTime DueDate {get; set;}
+    public bool IsCompleted {get; set;}
+}
